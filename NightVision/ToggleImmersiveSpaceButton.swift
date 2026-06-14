c549a2ed8b66ffa753c8b41d6d501527ce5f9aff
David Ernst <git@dsernst.com>
2026-06-14 13:30:41 -0700

    Main button: 'Enable/disable Night Vision'

--- NightVision/ToggleImmersiveSpaceButton.swift
+++ NightVision/ToggleImmersiveSpaceButton.swift
@@ -49,7 +49,7 @@ struct ToggleImmersiveSpaceButton: View {
                 }
             }
         } label: {
-            Text(appModel.immersiveSpaceState == .open ? "Hide Immersive Space" : "Show Immersive Space")
+            Text(appModel.immersiveSpaceState == .open ? "Disable Night Vision" : "Enable Night Vision")
         }
         .disabled(appModel.immersiveSpaceState == .inTransition)
         .animation(.none, value: 0)
